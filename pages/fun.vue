6b867a2e18d14feccd7fc3e2b2da0f4fc8a9df57
Michael Corning <michael@secours.io>
2021-02-05 21:15:31 -0800

    initial commit

---
+++ pages/fun.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        You can't test what you don't understand.
+        <footer>
+          <small>
+            <em>&mdash;Mpc</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
